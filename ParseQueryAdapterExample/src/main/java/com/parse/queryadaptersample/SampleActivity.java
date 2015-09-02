ad09631a0a1d14504ce6fbb477dd258574133bdf
Lukas Koebis <lukask@fb.com>
2015-09-01 17:42:18 -0700

    Added RecyclerView.Adapter adapter; sample project

---
+++ ParseQueryAdapterExample/src/main/java/com/parse/queryadaptersample/SampleActivity.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2014, Parse, LLC. All rights reserved.
+ *
+ *  You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ *  copy, modify, and distribute this software in source code or binary form for use
+ *  in connection with the web services and APIs provided by Parse.
+ *
+ *  As with any software that integrates with the Parse platform, your use of
+ *  this software is subject to the Parse Terms of Service
+ *  [https://www.parse.com/about/terms]. This copyright notice shall be
+ *  included in all copies or substantial portions of the software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.parse.queryadaptersample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.parse.ParseQueryRecyclerViewAdapter;
+import com.parse.ParseUser;
+import com.parse.queryadaptersample.R;
+
+/**
+ * Shows the user profile. This simple activity can function regardless of whether the user
+ * is currently logged in.
+ */
+public class SampleActivity extends Activity {
+
+  private RecyclerView recyclerView;
+
+  private ParseUser currentUser;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity);
+
+    recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+    ParseQueryRecyclerViewAdapter adapter = new ParseQueryRecyclerViewAdapter(this, "TestClass");
+    adapter.loadObjects();
+
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+  }
+}
